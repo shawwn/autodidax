0ec0a0078c44d51cd1bd29de8efeac6f63529c8a
Shawn Presser <shawnpresser@gmail.com>
2022-06-01 23:04:46 -0500

    Add jaxprs

--- autodidax/core.py
+++ autodidax/core.py
@@ -700,3 +700,493 @@ jacfwd(f, np.arange(3.))
 # -
 
 # That's it for `jvp` and `vmap`!
+
+
+# ## Part 2: Jaxprs
+#
+# The next transformations on the horizon are `jit` for just-in-time
+# compilation and `vjp` for reverse-mode autodiff.  (`grad` is just a small
+# wrapper around `vjp`.) Whereas `jvp` and `vmap` only needed each `Tracer` to
+# carry a little bit of extra context, for both `jit` and `vjp` we need much
+# richer context: we need to represent _programs_. That is, we need jaxprs!
+#
+# Jaxprs are JAX's internal intermediate representation of programs. They are
+# explicitly typed, functional, first-order, and in ANF form. We need a
+# program representation for `jit` because the purpose of `jit` is to stage
+# computation out of Python. For any computation we want to stage out, we need
+# to be able to represent it as data, and build it up as we trace a Python
+# function. Similarly, `vjp` needs a way to represent the computation for the
+# backward pass of reverse-mode autodiff. We use the same jaxpr program
+# representation for both needs.
+#
+# (Building a program representation is the most
+# [free](https://en.wikipedia.org/wiki/Free_object) kind of
+# trace-transformation, and so except for issues around handling native Python
+# control flow, any transformation could be implemented by first tracing to a
+# jaxpr and then interpreting the jaxpr.)
+
+# ### Jaxpr data structures
+#
+# The jaxpr term syntax is roughly:
+#
+# ```
+# jaxpr ::=
+#   { lambda <binder> , ... .
+#     let <eqn>
+#         ...
+#     in ( <atom> , ... ) }
+#
+# binder ::= <var>:<array_type>
+# var ::= a | b | c | ...
+# atom ::= <var> | <literal>
+# literal ::= <int32> | <int64> | <float32> | <float64>
+#
+# eqn ::= <binder> , ... = <primitive> [ <params> ] <atom> , ...
+# ```
+#
+# The syntax of types is:
+#
+# ```
+# jaxpr_type ::= [ <array_type> , ... ] -> [ <array_type> , ... ]
+# array_type ::= <dtype>[<shape>]
+# dtype ::= f32 | f64 | i32 | i64
+# shape ::= <int> , ...
+# ```
+#
+# How do we represent these as Python data structures? We reuse ShapedArrays to
+# represent types, and we can represent the term syntax with a few Python
+# structs:
+
+# +
+from typing import Set
+
+class Var:
+  aval: ShapedArray
+  def __init__(self, aval): self.aval = aval
+
+class Lit:
+  val: Any
+  aval: ShapedArray
+
+  def __init__(self, val):
+    self.aval = aval = raise_to_shaped(get_aval(val))
+    self.val = np.array(val, aval.dtype)
+
+Atom = Union[Var, Lit]
+
+class JaxprEqn(NamedTuple):
+  primitive: Primitive
+  inputs: List[Atom]
+  params: Dict[str, Any]
+  out_binders: List[Var]
+
+class Jaxpr(NamedTuple):
+  in_binders: List[Var]
+  eqns: List[JaxprEqn]
+  outs: List[Atom]
+
+  def __hash__(self): return id(self)
+  __eq__ = op.is_
+
+def raise_to_shaped(aval):
+  return ShapedArray(aval.shape, aval.dtype)
+
+
+# -
+
+# Type-checking a jaxpr involves checking that there are no unbound variables,
+# that variables are only bound once, and that for each equation the type of
+# the primitive application matches the type of the output binders.
+
+# +
+class JaxprType(NamedTuple):
+  in_types:  List[ShapedArray]
+  out_types: List[ShapedArray]
+
+  def __repr__(self):
+    in_types = ', '.join(aval.str_short() for aval in self.in_types)
+    out_types = ', '.join(aval.str_short() for aval in self.out_types)
+    return f'({in_types}) -> ({out_types})'
+
+def typecheck_jaxpr(jaxpr: Jaxpr) -> JaxprType:
+  env: Set[Var] = set()
+
+  for v in jaxpr.in_binders:
+    if v in env: raise TypeError
+    env.add(v)
+
+  for eqn in jaxpr.eqns:
+    in_types = [typecheck_atom(env, x) for x in eqn.inputs]
+    out_types = abstract_eval_rules[eqn.primitive](*in_types, **eqn.params)
+    for out_binder, out_type in zip(eqn.out_binders, out_types):
+      if not out_type == out_binder.aval: raise TypeError
+    for out_binder in eqn.out_binders:
+      if out_binder in env: raise TypeError
+      env.add(out_binder)
+
+  in_types = [v.aval for v in jaxpr.in_binders]
+  out_types = [typecheck_atom(env, x) for x in jaxpr.outs]
+  return JaxprType(in_types, out_types)
+
+def typecheck_atom(env: Set[Var], x: Atom) -> ShapedArray:
+  if isinstance(x, Var):
+    if x not in env: raise TypeError("unbound variable")
+    return x.aval
+  elif isinstance(x, Lit):
+    return raise_to_shaped(get_aval(x.val))
+  else:
+    assert False
+
+
+# -
+
+# We can apply the function represented by a jaxpr to arguments with a simple
+# interpreter.
+
+# +
+def eval_jaxpr(jaxpr: Jaxpr, args: List[Any]) -> List[Any]:
+  env: Dict[Var, Any] = {}
+
+  def read(x: Atom) -> Any:
+    return env[x] if type(x) is Var else x.val
+
+  def write(v: Var, val: Any) -> None:
+    assert v not in env  # single-assignment
+    env[v] = val
+
+  map(write, jaxpr.in_binders, args)
+  for eqn in jaxpr.eqns:
+    in_vals = map(read, eqn.inputs)
+    outs = bind(eqn.primitive, *in_vals, **eqn.params)
+    map(write, eqn.out_binders, outs)
+  return map(read, jaxpr.outs)
+
+def jaxpr_as_fun(jaxpr: Jaxpr):
+  return lambda *args: eval_jaxpr(jaxpr, args)
+
+
+# -
+
+# By using `bind` in the interpreter, this interpreter itself is traceable.
+
+# ### Building jaxprs with tracing
+#
+# Now that we have jaxprs as a data structure, we need ways to produce these
+# from tracing Python code. In general there are two variants of how we trace to
+# a jaxpr; `jit` uses one and `vjp` uses the other. We'll start with the one
+# used by `jit`, which is also used by control flow primitives like `lax.cond`,
+# `lax.while_loop`, and `lax.scan`.
+
+# +
+def split_list(lst: List[Any], n: int) -> Tuple[List[Any], List[Any]]:
+  assert 0 <= n <= len(lst)
+  return lst[:n], lst[n:]
+
+def partition_list(bs: List[bool], l: List[Any]) -> Tuple[List[Any], List[Any]]:
+  assert len(bs) == len(l)
+  lists = lst1, lst2 = [], []
+  for b, x in zip(bs, l):
+    lists[b].append(x)
+  return lst1, lst2
+
+
+# +
+# NB: the analogous class in JAX is called 'DynamicJaxprTracer'
+class JaxprTracer(Tracer):
+  __slots__ = ['aval']
+  aval: ShapedArray
+
+  def __init__(self, trace, aval):
+    self._trace = trace
+    self.aval = aval
+
+# NB: the analogous class in JAX is called 'DynamicJaxprTrace'
+class JaxprTrace(Trace):
+  def new_arg(self, aval: ShapedArray) -> JaxprTracer:
+    aval = raise_to_shaped(aval)
+    tracer = self.builder.new_tracer(self, aval)
+    self.builder.tracer_to_var[id(tracer)] = Var(aval)
+    return tracer
+
+  def get_or_make_const_tracer(self, val: Any) -> JaxprTracer:
+    tracer = self.builder.const_tracers.get(id(val))
+    if tracer is None:
+      tracer = self.builder.new_tracer(self, raise_to_shaped(get_aval(val)))
+      self.builder.add_const(tracer, val)
+    return tracer
+  pure = lift = get_or_make_const_tracer
+
+  def process_primitive(self, primitive, tracers, params):
+    avals_in = [t.aval for t in tracers]
+    avals_out = abstract_eval_rules[primitive](*avals_in, **params)
+    out_tracers = [self.builder.new_tracer(self, a) for a in avals_out]
+    inputs = [self.builder.getvar(t) for t in tracers]
+    outvars = [self.builder.add_var(t) for t in out_tracers]
+    self.builder.add_eqn(JaxprEqn(primitive, inputs, params, outvars))
+    return out_tracers
+
+  @property
+  def builder(self):
+    return self.main.global_data
+
+# NB: in JAX, we instead attach abstract eval rules to Primitive instances
+abstract_eval_rules = {}
+
+
+# -
+
+# Notice that we keep as interpreter-global data a builder object, which keeps
+# track of variables, constants, and eqns as we build up the jaxpr.
+
+class JaxprBuilder:
+  eqns: List[JaxprEqn]
+  tracer_to_var: Dict[int, Var]
+  const_tracers: Dict[int, JaxprTracer]
+  constvals: Dict[Var, Any]
+  tracers: List[JaxprTracer]
+
+  def __init__(self):
+    self.eqns = []
+    self.tracer_to_var = {}
+    self.const_tracers = {}
+    self.constvals = {}
+    self.tracers = []
+
+  def new_tracer(self, trace: JaxprTrace, aval: ShapedArray) -> JaxprTracer:
+    tracer = JaxprTracer(trace, aval)
+    self.tracers.append(tracer)
+    return tracer
+
+  def add_eqn(self, eqn: JaxprEqn) -> None:
+    self.eqns.append(eqn)
+
+  def add_var(self, tracer: JaxprTracer) -> Var:
+    assert id(tracer) not in self.tracer_to_var
+    var = self.tracer_to_var[id(tracer)] = Var(tracer.aval)
+    return var
+
+  def getvar(self, tracer: JaxprTracer) -> Var:
+    var = self.tracer_to_var.get(id(tracer))
+    assert var is not None
+    return var
+
+  def add_const(self, tracer: JaxprTracer, val: Any) -> Var:
+    var = self.add_var(tracer)
+    self.const_tracers[id(val)] = tracer
+    self.constvals[var] = val
+    return var
+
+  def build(self, in_tracers: List[JaxprTracer], out_tracers: List[JaxprTracer]
+            ) -> Tuple[Jaxpr, List[Any]]:
+    constvars, constvals = unzip2(self.constvals.items())
+    t2v = lambda t: self.tracer_to_var[id(t)]
+    in_binders = constvars + [t2v(t) for t in in_tracers]
+    out_vars = [t2v(t) for t in out_tracers]
+    jaxpr = Jaxpr(in_binders, self.eqns, out_vars)
+    typecheck_jaxpr(jaxpr)
+    jaxpr, constvals = _inline_literals(jaxpr, constvals)
+    return jaxpr, constvals
+
+
+def _inline_literals(jaxpr: Jaxpr, consts: List[Any]) -> Tuple[Jaxpr, List[Any]]:
+  const_binders, other_binders = split_list(jaxpr.in_binders, len(consts))
+  scalars = [type(x) in jax_types and not get_aval(x).shape for x in consts]
+  new_const_binders, lit_binders = partition_list(scalars, const_binders)
+  new_consts, lit_vals = partition_list(scalars, consts)
+  literals = dict(zip(lit_binders, map(Lit, lit_vals)))
+  new_eqns = [JaxprEqn(eqn.primitive, [literals.get(x, x) for x in eqn.inputs],
+                       eqn.params, eqn.out_binders) for eqn in jaxpr.eqns]
+  new_outs = [literals.get(x, x) for x in jaxpr.outs]
+  new_jaxpr = Jaxpr(new_const_binders + other_binders, new_eqns, new_outs)
+  typecheck_jaxpr(new_jaxpr)
+  return new_jaxpr, new_consts
+
+
+# The rules we need for `JaxprTrace.process_primitive` are essentially typing
+# rules for primitive applications: given the primitive, its parameters, and
+# types for the inputs, the rule must produce a type for the output, which is
+# then packaged with the output `JaxprTracer`. We can use abstract evaluation
+# rules for this same purpose, even though they can be more general (since
+# abstract evaluation rules must accept ConcreteArray inputs, and since they
+# need only return an upper bound on the set of possible outputs, they can
+# produce ConcreteArray outputs as well). We'll reuse these abstract evaluation
+# rules for the other jaxpr-producing trace machinery, where the potential extra
+# generality is useful.
+
+# +
+def binop_abstract_eval(x: ShapedArray, y: ShapedArray) -> List[ShapedArray]:
+  if not isinstance(x, ShapedArray) or not isinstance(y, ShapedArray):
+    raise TypeError
+  if raise_to_shaped(x) != raise_to_shaped(y): raise TypeError
+  return [ShapedArray(x.shape, x.dtype)]
+
+abstract_eval_rules[add_p] = binop_abstract_eval
+abstract_eval_rules[mul_p] = binop_abstract_eval
+
+def compare_abstract_eval(x: ShapedArray, y: ShapedArray) -> List[ShapedArray]:
+  if not isinstance(x, ShapedArray) or not isinstance(y, ShapedArray):
+    raise TypeError
+  if x.shape != y.shape: raise TypeError
+  return [ShapedArray(x.shape, np.dtype('bool'))]
+abstract_eval_rules[greater_p] = compare_abstract_eval
+abstract_eval_rules[less_p] = compare_abstract_eval
+
+def vectorized_unop_abstract_eval(x: ShapedArray) -> List[ShapedArray]:
+  return [ShapedArray(x.shape, x.dtype)]
+
+abstract_eval_rules[sin_p] = vectorized_unop_abstract_eval
+abstract_eval_rules[cos_p] = vectorized_unop_abstract_eval
+abstract_eval_rules[neg_p] = vectorized_unop_abstract_eval
+
+def reduce_sum_abstract_eval(x: ShapedArray, *, axis: int) -> List[ShapedArray]:
+  new_shape = [d for i, d in enumerate(x.shape) if i != axis]
+  return [ShapedArray(tuple(new_shape), x.dtype)]
+abstract_eval_rules[reduce_sum_p] = reduce_sum_abstract_eval
+
+def broadcast_abstract_eval(x: ShapedArray, *, shape: Sequence[int],
+                            axes: Sequence[int]) -> List[ShapedArray]:
+  return [ShapedArray(tuple(shape), x.dtype)]
+abstract_eval_rules[broadcast_p] = broadcast_abstract_eval
+# -
+
+# To check our implementation of jaxprs, we can add a `make_jaxpr`
+# transformation and a pretty-printer:
+
+# +
+from functools import lru_cache
+
+@lru_cache()  # ShapedArrays are hashable
+def make_jaxpr_v1(f, *avals_in):
+  avals_in, in_tree = tree_flatten(avals_in)
+  f, out_tree = flatten_fun(f, in_tree)
+
+  builder = JaxprBuilder()
+  with new_main(JaxprTrace, builder) as main:
+    trace = JaxprTrace(main)
+    tracers_in = [trace.new_arg(aval) for aval in avals_in]
+    outs = f(*tracers_in)
+    tracers_out = [full_raise(trace, out) for out in outs]
+    jaxpr, consts = builder.build(tracers_in, tracers_out)
+  return jaxpr, consts, out_tree()
+
+
+# + tags=["hide-input"]
+from typing import DefaultDict
+from collections import defaultdict
+import string
+
+class PPrint:
+  lines: List[Tuple[int, str]]
+
+  def __init__(self, lines):
+    self.lines = lines
+
+  def indent(self, indent: int) -> 'PPrint':
+    return PPrint([(indent + orig_indent, s) for orig_indent, s in self.lines])
+
+  def __add__(self, rhs: 'PPrint') -> 'PPrint':
+    return PPrint(self.lines + rhs.lines)
+
+  def __rshift__(self, rhs: 'PPrint') -> 'PPrint':
+    if not rhs.lines: return self
+    if not self.lines: return rhs
+    indent, s = self.lines[-1]
+    indented_block = rhs.indent(indent + len(s))
+    common_line = s + ' ' * rhs.lines[0][0] + rhs.lines[0][1]
+    return PPrint(self.lines[:-1]
+                  + [(indent, common_line)]
+                  + indented_block.lines[1:])
+
+  def __str__(self) -> str:
+    return '\n'.join(' ' * indent + s for indent, s in self.lines)
+
+def pp(s: Any) -> PPrint:
+  return PPrint([(0, line) for line in str(s).splitlines()])
+
+def vcat(ps: List[PPrint]) -> PPrint:
+  return sum(ps, pp(''))
+
+def pp_jaxpr(jaxpr: Jaxpr) -> PPrint:
+  namegen = (''.join(s) for r in it.count(1)
+             for s in it.permutations(string.ascii_lowercase, r))
+  names = defaultdict(lambda: next(namegen))
+  in_binders = ', '.join(var_str(names, x) for x in jaxpr.in_binders)
+  eqns = vcat([pp_eqn(names, e) for e in jaxpr.eqns])
+  outs = ', '.join(names[v] if isinstance(v, Var) else str(v.val)
+                   for v in jaxpr.outs)
+  return (pp(f'{{ lambda {in_binders} .') +
+          ((pp('let ') >> eqns) + pp(f'in ( {outs} ) }}')).indent(2))
+
+def var_str(names: DefaultDict[Var, str], v: Var) -> str:
+  return f'{names[v]}:{v.aval.str_short()}'
+
+def pp_eqn(names: DefaultDict[Var, str], eqn: JaxprEqn) -> PPrint:
+  rule = pp_rules.get(eqn.primitive)
+  if rule:
+    return rule(names, eqn)
+  else:
+    lhs = pp(' '.join(var_str(names, v) for v in eqn.out_binders))
+    rhs = (pp(eqn.primitive.name) >> pp_params(eqn.params) >>
+           pp(' '.join(names[x] if isinstance(x, Var) else str(x.val)
+                       for x in eqn.inputs)))
+    return lhs >> pp(' = ') >> rhs
+
+def pp_params(params: Dict[str, Any]) -> PPrint:
+  items = sorted(params.items())
+  if items:
+    return pp(' [ ') >> vcat([pp(f'{k}={v}') for k, v in items]) >> pp(' ] ')
+  else:
+    return pp(' ')
+
+Jaxpr.__repr__ = lambda self: str(pp_jaxpr(self))
+pp_rules: Dict[Primitive, Callable[..., PPrint]] = {}
+# -
+
+jaxpr, consts, _ = make_jaxpr_v1(lambda x: 2. * x, raise_to_shaped(get_aval(3.)))
+print(jaxpr)
+print(typecheck_jaxpr(jaxpr))
+
+# But there's a limitation here: because of how `find_top_trace` operates by
+# data dependence, `make_jaxpr_v1` can't stage out all the primitive operations
+# performed by the Python callable it's given. For example:
+
+jaxpr, consts, _ = make_jaxpr_v1(lambda: mul(2., 2.))
+print(jaxpr)
+
+
+# This is precisely the issue that
+# [omnistaging](https://github.com/google/jax/pull/3370) fixed.
+# We want to ensure that the `JaxprTrace` started by `make_jaxpr` is always
+# applied, regardless of whether any inputs to `bind` are boxed in corresponding
+# `JaxprTracer` instances. We can achieve this by employing the `dynamic_trace`
+# global defined in Part 1:
+
+# +
+@contextmanager
+def new_dynamic(main: MainTrace):
+  global dynamic_trace
+  prev_dynamic_trace, dynamic_trace = dynamic_trace, main
+  try:
+    yield
+  finally:
+    dynamic_trace = prev_dynamic_trace
+
+@lru_cache()
+def make_jaxpr(f: Callable, *avals_in: ShapedArray,
+               ) -> Tuple[Jaxpr, List[Any], PyTreeDef]:
+  avals_in, in_tree = tree_flatten(avals_in)
+  f, out_tree = flatten_fun(f, in_tree)
+
+  builder = JaxprBuilder()
+  with new_main(JaxprTrace, builder) as main:
+    with new_dynamic(main):
+      trace = JaxprTrace(main)
+      tracers_in = [trace.new_arg(aval) for aval in avals_in]
+      outs = f(*tracers_in)
+      tracers_out = [full_raise(trace, out) for out in outs]
+      jaxpr, consts = builder.build(tracers_in, tracers_out)
+  return jaxpr, consts, out_tree()
+
+jaxpr, consts, _ = make_jaxpr(lambda: mul(2., 2.))
+print(jaxpr)
+
