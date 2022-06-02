3b57c2c8ea8e1684e2cb4683284fb922d53c7373
Shawn Presser <shawnpresser@gmail.com>
2022-06-02 01:31:25 -0500

    Implement the rest of autodidax

--- autodidax/__init__.py
+++ autodidax/__init__.py
@@ -1,19 +1,21 @@
 from . import test_util
-from .core import (
-  jax_types,
+# from .core import (
+#   jax_types,
+#
+#   impl_rules,
+#   add, add_p,
+#   mul, mul_p,
+#   neg, neg_p,
+#   sin, sin_p,
+#   cos, cos_p,
+#   reduce_sum, reduce_sum_p,
+#   greater, greater_p,
+#   less, less_p,
+#   transpose, transpose_p,
+#   broadcast, broadcast_p,
+#
+#   jvp_rules,
+#   jvp_v1,
+# )
 
-  impl_rules,
-  add, add_p,
-  mul, mul_p,
-  neg, neg_p,
-  sin, sin_p,
-  cos, cos_p,
-  reduce_sum, reduce_sum_p,
-  greater, greater_p,
-  less, less_p,
-  transpose, transpose_p,
-  broadcast, broadcast_p,
-
-  jvp_rules,
-  jvp_v1,
-)
+from .core import *
